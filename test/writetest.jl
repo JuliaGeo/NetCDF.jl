c7b5bd2d7aef6a8a64bb71189e61f9cc4be00c78
meggart <fabiangans@gmx.de>
2013-05-07 15:31:09 +0200

    removed deprecated tests again

--- test/writetest.jl
+++
@@ -1,8 +0,0 @@
-using netcdf
-lon=NcDim("longitude",[1:10])
-lat=NcDim("latitude",[1:20])
-rad=NcVar("Radiation",[lon,lat],{"units"=>"W/m^2"},Float32)
-nc=netcdf.create("test.nc",rad)
-x=rand(10,20);
-netcdf.putvar(nc,"Radiation",[1,1],x)
-netcdf.close(nc)
