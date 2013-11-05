28b43d6a33d7a5a98a023cb71ed95aaaee1aff07
Fabian Gans <fabiangans@gmx.de>
2013-11-05 20:47:22 +0100

    added windows dep

--- deps/build.jl
+++ deps/build.jl
@@ -3,7 +3,7 @@ using BinDeps
 @BinDeps.setup
 
 
-netcdf = library_dependency("netcdf", aliases = ["libnetcdf"])
+netcdf = library_dependency("netcdf", aliases = ["libnetcdf","libnetcdf-7"])
 
 
 @osx_only begin
@@ -17,7 +17,9 @@ end
 end
 
 @windows_only begin
-  provides (Binaries,URI("ftp://ftp.unidata.ucar.edu/pub/netcdf/contrib/win32/netcdf-4.1.1-win32-bin.zip"),netcdf,os=:Windows)
+  #provides (Binaries,URI("ftp://ftp.unidata.ucar.edu/pub/netcdf/contrib/win32/netcdf-4.1.1-win32-bin.zip"),netcdf,os=:Windows)
+  provides (Binaries,URI("http://www.paratools.com/Azure/NetCDF?action=AttachFile&do=get&target=paratools-netcdf-4.1.3.tar.gz",netcdf,os=:Windows)
+  push!(DL_LOAD_PATH, Pkg.dir("NetCDF/deps/netcdf-4.1.3/usr/bin/"))
 end
 
 provides(Sources, URI("ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.0.tar.gz"),netcdf)
