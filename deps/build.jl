f7938e7f6faca55eeb37fb611ab3084fda7ee4b2
meggart <fabiangans@gmx.de>
2013-11-20 14:01:32 +0100

    fixed bracket bug

--- deps/build.jl
+++ deps/build.jl
@@ -18,7 +18,7 @@ end
 
 #@windows_only begin
 #  #provides (Binaries,URI("ftp://ftp.unidata.ucar.edu/pub/netcdf/contrib/win32/netcdf-4.1.1-win32-bin.zip"),netcdf,os=:Windows)
-  provides (Binaries,URI("http://www.paratools.com/Azure/NetCDF/paratools-netcdf-4.1.3.tar.gz",netcdf,os=:Windows)
+  provides (Binaries,URI("http://www.paratools.com/Azure/NetCDF/paratools-netcdf-4.1.3.tar.gz"),netcdf,os=:Windows)
   push!(DL_LOAD_PATH, joinpath(Pkg.dir(),"NetCDF","deps","netcdf-4.1.3","bin"))
 #end
 
