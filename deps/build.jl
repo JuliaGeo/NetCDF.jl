a97c4bdac18990e5a079d1286f2a062c13961fbd
Fabian Gans <fgans@bgc-jena.mpg.de>
2014-04-28 17:37:15 +0200

    added windows macro again

--- deps/build.jl
+++ deps/build.jl
@@ -3,24 +3,23 @@ using BinDeps
 @BinDeps.setup
 
 
-netcdf = library_dependency("netcdf", aliases = ["libnetcdf","libnetcdf-7"])
-
-
 @osx_only begin
-  using Homebrew
-  provides( Homebrew.HB, "netcdf", netcdf, os = :Darwin )
+    using Homebrew
+    netcdf = library_dependency("libnetcdf", aliases = ["libnetcdf.7"])
+    provides( Homebrew.HB, "netcdf", netcdf, os = :Darwin )
 end
 
 @linux_only begin
-  provides(AptGet, {"libnetcdf-dev"=>netcdf})
-  provides(Yum, {"netcdf-devel"=>netcdf})
+    netcdf = library_dependency("libnetcdf", aliases = ["libnetcdf-7"])
+    provides(AptGet, {"libnetcdf-dev"=>netcdf})
+    provides(Yum, {"netcdf-devel"=>netcdf})
 end
 
-#@windows_only begin
-#  #provides (Binaries,URI("ftp://ftp.unidata.ucar.edu/pub/netcdf/contrib/win32/netcdf-4.1.1-win32-bin.zip"),netcdf,os=:Windows)
-  provides (Binaries,URI("http://www.paratools.com/Azure/NetCDF/paratools-netcdf-4.1.3.tar.gz"),netcdf,os=:Windows)
-  push!(DL_LOAD_PATH, joinpath(Pkg.dir(),"NetCDF","deps","netcdf-4.1.3","bin"))
-#end
+@windows_only begin
+    netcdf = library_dependency("netcdf")
+    provides (Binaries,URI("http://www.paratools.com/Azure/NetCDF/paratools-netcdf-4.1.3.tar.gz"),netcdf,os=:Windows)
+    push!(DL_LOAD_PATH, joinpath(Pkg.dir(),"NetCDF","deps","netcdf-4.1.3","bin"))
+end
 
 provides(Sources, URI("ftp://ftp.unidata.ucar.edu/pub/netcdf/netcdf-4.3.0.tar.gz"),netcdf)
 
