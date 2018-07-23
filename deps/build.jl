350510755fe7718a11262d19e8dc139fd5b68403
Steven G. Johnson <stevenj@mit.edu>
2018-07-23 13:06:53 -0400

    switch to CondaBinDeps

--- deps/build.jl
+++ deps/build.jl
@@ -1,11 +1,11 @@
 using BinDeps
-using Conda
+import CondaBinDeps
 
 @BinDeps.setup
 libnetcdf = library_dependency("libnetcdf", aliases = ["libnetcdf4","libnetcdf-7","netcdf"])
 
-#Conda.add_channel("conda-forge")
-provides(Conda.Manager, "libnetcdf", libnetcdf)
+#CondaBinDeps.Conda.add_channel("conda-forge")
+provides(CondaBinDeps.Manager, "libnetcdf", libnetcdf)
 provides(AptGet, "libnetcdf-dev", libnetcdf, os = :Linux)
 provides(Yum, "netcdf-devel", libnetcdf, os = :Linux)
 
