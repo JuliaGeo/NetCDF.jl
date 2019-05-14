0be8a2a862c437e058620ff74e6ab35f85eca0b3
autodocs <autodocs>
2019-05-14 17:11:26 +0000

    build based on 98feace

--- dev/search_index.js
+++ dev/search_index.js
@@ -125,7 +125,7 @@ var documenterSearchIndex = {"docs": [
     "page": "High-level interface",
     "title": "NetCDF.ncread!",
     "category": "function",
-    "text": "ncread!(filename, varname, d)\n\nreads the values of the variable varname from file filename and writes the results to the pre-allocated array d\n\nKeyword arguments\n\nstart Vector of length ndim(v) setting the starting index for each dimension\ncount Vector of length ndim(v) setting the count of values to be read along each dimension. The value -1 is treated as a special case to read all values from this dimension\n\nExample\n\nTo read the second slice of a 3D NetCDF variable one can write:\n\nd = zeros(10,10,1) ncread!(\"filename\",\"varname\", d, start=[1,1,2], count = [-1,-1,1])\n\n\n\n\n\n"
+    "text": "ncread!(filename, varname, d)\n\nreads the values of the variable varname from file filename and writes the results to the pre-allocated array d\n\nKeyword arguments\n\nstart Vector of length ndim(v) setting the starting index for each dimension\ncount Vector of length ndim(v) setting the count of values to be read along each dimension. The value -1 is treated as a special case to read all values from this dimension\n\nExample\n\nTo read the second slice of a 3D NetCDF variable one can write:\n\nd = zeros(10,10,1)\nncread!(\"filename\",\"varname\", d, start=[1,1,2], count = [-1,-1,1])\n\n\n\n\n\n"
 },
 
 {
@@ -189,7 +189,7 @@ var documenterSearchIndex = {"docs": [
     "page": "High-level interface",
     "title": "NetCDF.nccreate",
     "category": "function",
-    "text": "nccreate (filename, varname, dimensions ...)\n\nCreate a variable in an existing netcdf file or generates a new file. filename and varname are strings. After that follows a list of dimensions. Each dimension entry starts with a dimension name (a String), and may be followed by a dimension length, an array with dimension values or a Dict containing dimension attributes. Then the next dimension is entered and so on. Have a look at examples/high.jl for an example use.\n\n###Keyword arguments\n\natts Dict of attribute names and values to be assigned to the variable created\ngatts Dict of attribute names and values to be written as global attributes\ncompress Integer [0..9] setting the compression level of the file, only valid if mode=NC_NETCDF4\nt variable type, currently supported types are: const NCBYTE, NCCHAR, NCSHORT, NCINT, NCFLOAT, NCLONG, NC_DOUBLE\nmode file creation mode, only valid when new file is created, choose one of: NCNETCDF4, NCCLASSICMODEL, NC64BIT_OFFSET\n\n\n\n\n\n"
+    "text": "nccreate (filename, varname, dimensions ...)\n\nCreate a variable in an existing NetCDF file or generates a new file. filename and varname are strings. After that follows a list of dimensions. Each dimension entry starts with a dimension name (a String), and may be followed by a dimension length, an array with dimension values or a Dict containing dimension attributes. Then the next dimension is entered and so on. Have a look at examples/high.jl for an example use.\n\nKeyword arguments\n\natts Dict of attribute names and values to be assigned to the variable created\ngatts Dict of attribute names and values to be written as global attributes\ncompress Integer [0..9] setting the compression level of the file, only valid if mode=NC_NETCDF4\nt variable type, currently supported types are: const NC_BYTE, NC_CHAR, NC_SHORT, NC_INT, NC_FLOAT, NC_LONG, NC_DOUBLE\nmode file creation mode, only valid when new file is created, choose one of: NC_NETCDF4, NC_CLASSIC_MODEL, NC_64BIT_OFFSET\n\n\n\n\n\n"
 },
 
 {
@@ -285,7 +285,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Medium-level interface",
     "title": "NetCDF.putvar",
     "category": "function",
-    "text": "NetCDF.putvar(v::NcVar,vals::Array;start::Vector=ones(Int,length(size(vals))),count::Vector=[size(vals)...])\n\nWrites the values from the array vals to a netcdf file. v is the NcVar handle of the respective variable and vals an array with the same dimension as the variable in the netcdf file.\n\nKeyword arguments\n\nstart Vector of length ndim(v) setting the starting index for each dimension\ncount Vector of length ndim(v) setting the count of values to be read along each dimension. The value -1 is treated as a special case to read all values from this dimension\n\n\n\n\n\nNetCDF.putvar(v::NcVar, val, i...)\n\nWrites the value(s) val to the variable v while the indices are given in in an array-style indexing manner.\n\n\n\n\n\n"
+    "text": "NetCDF.putvar(v::NcVar,vals::Array;start::Vector=ones(Int,length(size(vals))),count::Vector=[size(vals)...])\n\nWrites the values from the array vals to a NetCDF file. v is the NcVar handle of the respective variable and vals an array with the same dimension as the variable in the NetCDF file.\n\nKeyword arguments\n\nstart Vector of length ndim(v) setting the starting index for each dimension\ncount Vector of length ndim(v) setting the count of values to be read along each dimension. The value -1 is treated as a special case to read all values from this dimension\n\n\n\n\n\nNetCDF.putvar(v::NcVar, val, i...)\n\nWrites the value(s) val to the variable v while the indices are given in in an array-style indexing manner.\n\n\n\n\n\n"
 },
 
 {
@@ -301,7 +301,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Medium-level interface",
     "title": "Creating files",
     "category": "section",
-    "text": "To create a netCDF file you first have to define the dimensions and variables that it is supposed to hold. As representations for netCDF dimensions and variables there are the predefined NcVar and NcDim types. An NcDim object is created by:NcDim(dimname, dimlength, atts=Dict{Any,Any}(), values=[], unlimited=false)here dimname is the dimension name, dimlength is the dimension length. The optional argument values is a 1D array of values that are written to the dimension variable and the optional argument atts is a Dict holding pairs of attribute names and values. Setting unlimited=true creates an unlimited dimension.After defining the dimensions, you can create NcVar objects withNcVar(varname , dimlist; atts=Dict{Any,Any}(), t=Float64, compress=-1)Here varname is the name of the variable, dimlist an array of type NcDim holding the dimensions associated to the variable, varattributes is a Dict holding pairs of attribute names and values. t is the data type that should be used for storing the variable.  You can either specify a Julia type(Int16, Int32, Float32, Float64) which will be translated to(NCSHORT, NCINT, NCFLOAT, NCDOUBLE) or directly specify one of the latter list. You can also set the compression level of the variable by setting compress to a number in the range 1..9 This has only an effect in netCDF4 files.Having defined the variables, the netCDF file can be created:NetCDF.create(filename, varlist, gatts=Dict{Any,Any}(),mode=NC_netCDF4)Here, filename is the name of the file to be created and varlist an array of NcVar holding the variables that should appear in the file. In the optional argument gatts you can specify a Dict containing global attributes and mode is the file type you want to create(NCnetCDF4, NCCLASSICMODEL or NC64BIT_OFFSET)."
+    "text": "To create a NetCDF file you first have to define the dimensions and variables that it is supposed to hold. As representations for NetCDF dimensions and variables there are the predefined NcVar and NcDim types. An NcDim object is created by:NcDim(dimname, dimlength, atts=Dict{Any,Any}(), values=[], unlimited=false)here dimname is the dimension name, dimlength is the dimension length. The optional argument values is a 1D array of values that are written to the dimension variable and the optional argument atts is a Dict holding pairs of attribute names and values. Setting unlimited=true creates an unlimited dimension.After defining the dimensions, you can create NcVar objects withNcVar(varname , dimlist; atts=Dict{Any,Any}(), t=Float64, compress=-1)Here varname is the name of the variable, dimlist an array of type NcDim holding the dimensions associated to the variable, varattributes is a Dict holding pairs of attribute names and values. t is the data type that should be used for storing the variable.  You can either specify a Julia type (Int16, Int32, Float32, Float64) which will be translated to (NC_SHORT, NC_INT, NC_FLOAT, NC_DOUBLE) or directly specify one of the latter list. You can also set the compression level of the variable by setting compress to a number in the range 1..9 This has only an effect in NetCDF4 files.Having defined the variables, the NetCDF file can be created:NetCDF.create(filename, varlist, gatts=Dict{Any,Any}(),mode=NC_NETCDF4)Here, filename is the name of the file to be created and varlist an array of NcVar holding the variables that should appear in the file. In the optional argument gatts you can specify a Dict containing global attributes and mode is the file type you want to create(NC_NETCDF4, NC_CLASSIC_MODEL or NC_64BIT_OFFSET)."
 },
 
 {
@@ -309,7 +309,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Medium-level interface",
     "title": "Miscellaneous",
     "category": "section",
-    "text": "once you have finished reading, writing or editing your files you can close the file withNetCDF.close(nc)If you just want to synchronize your changes to the disk, runNetCDF.sync(nc)where nc is a netCDF file handler."
+    "text": "once you have finished reading, writing or editing your files you can close the file withNetCDF.close(nc)If you just want to synchronize your changes to the disk, runNetCDF.sync(nc)where nc is a NetCDF file handle."
 },
 
 {
@@ -333,7 +333,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Medium-level interface",
     "title": "NetCDF.create",
     "category": "function",
-    "text": "NetCDF.create(name::String,varlist::Array{NcVar};gatts::Dict=Dict{Any,Any}(),mode::UInt16=NC_NETCDF4)\n\nCreates a new NetCDF file. Here, name  is the name of the file to be created and varlist an array of NcVar holding the variables that should appear in the file.\n\nKeyword arguments\n\ngatts a Dict containing global attributes of the NetCDF file\nmode NetCDF file type (NCNETCDF4, NCCLASSICMODEL or NC64BITOFFSET), defaults to NCNETCDF4\n\n\n\n\n\n"
+    "text": "NetCDF.create(name::String,varlist::Array{NcVar};gatts::Dict=Dict{Any,Any}(),mode::UInt16=NC_NETCDF4)\n\nCreates a new NetCDF file. Here, name  is the name of the file to be created and varlist an array of NcVar holding the variables that should appear in the file.\n\nKeyword arguments\n\ngatts a Dict containing global attributes of the NetCDF file\nmode NetCDF file type (NC_NETCDF4, NC_CLASSIC_MODEL or NC_64BIT_OFFSET), defaults to NC_NETCDF4\n\n\n\n\n\n"
 },
 
 {
@@ -346,23 +346,23 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "strings/#",
-    "page": "Short note on reading and writing NCCHAR and NCSTRING variables",
-    "title": "Short note on reading and writing NCCHAR and NCSTRING variables",
+    "page": "Short note on reading and writing NC_CHAR and NC_STRING variables",
+    "title": "Short note on reading and writing NC_CHAR and NC_STRING variables",
     "category": "page",
     "text": ""
 },
 
 {
-    "location": "strings/#Short-note-on-reading-and-writing-NC*CHAR-and-NC*STRING-variables-1",
-    "page": "Short note on reading and writing NCCHAR and NCSTRING variables",
-    "title": "Short note on reading and writing NCCHAR and NCSTRING variables",
+    "location": "strings/#Short-note-on-reading-and-writing-NC_CHAR-and-NC_STRING-variables-1",
+    "page": "Short note on reading and writing NC_CHAR and NC_STRING variables",
+    "title": "Short note on reading and writing NC_CHAR and NC_STRING variables",
     "category": "section",
     "text": "There are two common types for storing String data in NetCDF variables. The first is NC_CHAR, where a 1D array of strings is stored in a 2D char** array. Here the user must define the maximum string length and add a respective NetCDF dimension. Since NetCDF4 there is the NC_STRING data type that allows  the direct definition of String variables so that an N-dimensional String array directly maps to an N-dimensional  array in the NetCDF file structure."
 },
 
 {
     "location": "strings/#NC_STRING-variables-1",
-    "page": "Short note on reading and writing NCCHAR and NCSTRING variables",
+    "page": "Short note on reading and writing NC_CHAR and NC_STRING variables",
     "title": "NC_STRING variables",
     "category": "section",
     "text": "In this package, the Julia type String maps to the NC_STRING datatype, which means that creating a variable with any ofnccreate(filename, varname, dims..., t=String)orNcVar(varname,dims,t=String)will result in a NetCDF variable of type NC_STRING. You can directly write an Array{String} of matching shape to these variables. Similarly, calling ncread or NetCDF.readvar on any of these variables will return an Array{String}"
@@ -370,7 +370,7 @@ var documenterSearchIndex = {"docs": [
 
 {
     "location": "strings/#NC_CHAR-variables-1",
-    "page": "Short note on reading and writing NCCHAR and NCSTRING variables",
+    "page": "Short note on reading and writing NC_CHAR and NC_STRING variables",
     "title": "NC_CHAR variables",
     "category": "section",
     "text": "Dealing with NC_CHAR variables is a bit more complicated. This has 2 reasons. First, the dimensions of the NetCDF variables do not match the dimensions of the resulting string array because of the additional str_len (or similar) axis that is introduced in the NetCDF file. So an n-dimensional String-Array maps to an (n+1)-dimensional NC_CHAR array.Second, historically the NC_CHAR type has been used to store compressed data, too. So it is not always desirable to automatically convert these char arrays to strings. Anyhow, here is how you can deal with these variable types:Assume you have a NetCDF variable of type NC_CHAR of dimensions (str_len: 10, axis2: 20). Calling x=ncread(...) or x=readvar(...) on this variable will return an Array{ASCIIChar,2} with size (10,20) as it is represented on disk. The ASCIIChar type is a small wrapper around UInt8, needed for dispatch. You can simply convert them to either Char or UInt8 using the convert function. The returned array can either be used directly (if it is numeric maybe use reinterpret(UInt8,x)) or convert them to a Vector{String} by callingy=nc_char2string(x)which will return a string vector of length 20.An example for creating NC_CHAR and writing variables would be the following:nccreate(filename,varname,\"str_len\",20,\"DimValues\",5,t=NC_CHAR)\nxs = [\"a\",\"bb\",\"ccc\",\"dddd\",\"eeeee\"]\nncwrite(filename,varname,nc_string2char(xs))The call of string2char will convert the Vector{String} to a Matrix{UInt8}. which can be written to the NetCDF file."
