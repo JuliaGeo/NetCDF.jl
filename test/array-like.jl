72adc7f27b4d4cf6c73d64df5502ba122fa26770
Fabian Gans <fgans@bgc-jena.mpg.de>
2020-01-14 10:25:57 +0100

    Use DiskArrays to implement AbstractArray interface

--- test/array-like.jl
+++ test/array-like.jl
@@ -2,13 +2,13 @@ NetCDF.open(fn1, "v1", mode = NC_WRITE) do v1
     @test isa(v1, NetCDF.NcVar{Float64,3,6})
     @test v1[1, 1, 1] == x1[1, 1, 1]
 
-    @test dropdims(v1[2, :, 2], dims = (1, 3)) == x1[2, :, 2]
+  @test v1[2,:,2] == x1[2,:,2]
 
-    @test dropdims(v1[:, 3, 1], dims = (2, 3)) == x1[:, 3, 1]
-    inds = bitrand(size(x1))
-    @test v1[inds] == x1[inds]
+  @test v1[:,3,1] == x1[:,3,1]
+  inds = bitrand(size(x1))
+  @test v1[inds] == x1[inds]
 
-    xnew = rand(Float64, size(x1))
-    v1[:, :, :] = xnew
-    @test v1 == xnew
+  xnew = rand(Float64,size(x1))
+  v1[:,:,:] = xnew
+  @test v1[:,:,:] == xnew
 end
