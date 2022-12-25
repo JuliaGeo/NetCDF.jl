90c0440f87cdae2b682f6930efbab5cb8c2752e5
Peter Shin <petershintech@gmail.com>
2022-12-25 17:46:17 +1100

    Insert comma and replace one with you to be more clear and natural

--- src/NetCDF.jl
+++ src/NetCDF.jl
@@ -1112,7 +1112,7 @@ Writes the array `x` to the file `fil` and variable `vname`.
 ### Keyword arguments
 
 * `start` Vector of length `ndim(v)` setting the starting index for writing for each dimension
-* `count` Vector of length `ndim(v)` setting the count of values to be written along each dimension. The value -1 is treated as a special case to write all values from this dimension. This is usually inferred by the given array size.
+* `count` Vector of length `ndim(v)` setting the count of values to be written along each dimension. The value -1 is treated as a special case to write all values along the dimension. This is usually inferred by the given array size.
 """
 function ncwrite(
     x::Array,
